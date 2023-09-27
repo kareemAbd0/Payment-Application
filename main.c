87fc385ab19eff8f86e3a2dae95238fcb8f68e75
Kareem Abdo <kareemabdo.7781@yahoo.com>
2023-09-27 21:08:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,19 +9,6 @@
 
 int main() {
 
-//    ST_cardData_t  card ;
-//    getCardHolderName(&card);
-//    getCardExpiryDate(&card);
-//    getCardPAN(&card);
-//    ST_terminalData_t terminal ;
-//    getTransactionAmount(&terminal);
-//    getTransactionDate(&terminal);
-//    setMaxAmount(&terminal);
-//    ST_transaction_t transaction;
-//    transaction.terminalData = terminal;
-//    transaction.cardHolderData = card;
-    //transactions[0].transState = APPROVED;
-   // transactions[0].transactionSequenceNumber = 124532454;
 
     strcpy((char*)accountsDb[0].primaryAccountNumber,"4443345543548856568");
     accountsDb[0].balance = 1000000;
@@ -33,21 +20,6 @@ int main() {
 
 
 
-
-//    EN_transState_t transactionStates = receiveTransactionData(&transaction  );
-//
-//    if(transactionStates == DECLINED_STOLEN_CARD)
-//    {
-//        printf("card not found");
-//    }else if (transactionStates == DECLINED_INSUFFECIENT_FUND)
-//   {
-//       printf("not enough balance");
-//   } else if (transactionStates == APPROVED) {
-//        printf("transaction completed");
-//    } else if (transactionStates == INTERNAL_SERVER_ERROR)
-//    {
-//        printf("server error");
-//    }
 
     return 0;
 }
